bac6695ea9bf6ce988b50fb5e8821862c85cf7d4
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-07-11 21:04:23 +0800

    enchance Loading animation

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -1,6 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import styled, { keyframes } from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 import loadingImage from '../Assets/Media/choose.svg';
+
+const blurIn = keyframes`
+  0% {
+    filter: blur(100px);
+  }
+  100% {
+    filter: blur(0px);
+    color: black;
+  }
+`;
 
 const LoadingWrapper = styled.div`
   background: url(${loadingImage}) no-repeat left center/cover;
@@ -10,35 +20,45 @@ const LoadingWrapper = styled.div`
   width: calc(100vw + 60px);
   height: calc(100vh + 60px);
   z-index: -1;
-  filter: blur(0px);
+  filter: blur(100px);
+  animation: 2.9s linear forwards ${blurIn};
 `;
 
 const fadeIn = keyframes`
   0% {
-    opacity: .05;
-  }
-  100% {
     opacity: 1;
   }
+  100% {
+    opacity: 0.05;
+  }
+`;
+
+const loadingAnimation = ({ time, alternate }) => css`
+  ${time} linear ${alternate} ${fadeIn};
 `;
 
 const LoadingMessage = styled.h2`
   text-align: center;
   font-size: 5rem;
   color: #fff;
-  animation: 1s linear infinite alternate ${fadeIn};
+  opacity: 0;
+  animation: ${({ wait }) => {
+    if (wait > 200) {
+      return loadingAnimation({ time: `2s`, alternate: `alternate infinite` });
+    }
+    return loadingAnimation({ time: `3s` });
+  }};
 `;
-
-// const scale = (num, in_min, in_max, out_min, out_max) => {
-//   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
-// };
 
 function Loading() {
   const [loadingProgress, setLoadingProgress] = useState(0);
+  const [wait, setWait] = useState(0);
   useEffect(() => {
     let count = 0;
     const timer = setInterval(() => {
-      if (count > 99) return;
+      if (count > 99) {
+        return setWait((prev) => prev + 1);
+      }
       count++;
       setLoadingProgress((prev) => prev + 1);
     }, 15);
@@ -46,10 +66,13 @@ function Loading() {
       clearInterval(timer);
     };
   }, []);
+
   return (
     <React.Fragment>
       <LoadingWrapper />
-      <LoadingMessage>{`Loading ${loadingProgress}%...`}</LoadingMessage>
+      <LoadingMessage
+        wait={wait}
+      >{`Loading ${loadingProgress}%...`}</LoadingMessage>
     </React.Fragment>
   );
 }
