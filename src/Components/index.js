8626a0bed147c74b4ac6bd11167166aef5abbfe6
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-07-12 17:46:09 +0800

    export Body & Button components

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,5 +1,7 @@
+export { default as Body } from './Body/Body';
 export { default as Loading } from './Loading/Loading';
 export { default as ProgressBar } from './ProgressBar/ProgressBar';
 export { default as QuizTitle } from './QuizTitle/QuizTitle';
 export { default as QuizItems } from './QuizItems/QuizItems';
 export { default as QuizChoices } from './QuizChoices/QuizChoices';
+export { default as Button } from './Button/Button';
