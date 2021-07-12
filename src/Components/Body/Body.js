80717ebcaba42c5c7dd66792af64dbaa42ed80fb
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-07-12 19:32:28 +0800

    add default font color for body

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const Body = styled.main`
   background-color: ${(props) => props.theme.primaryBgColor};
+  color: ${(props) => props.theme.primaryFontColor};
   min-height: 100vh;
 `;
 
