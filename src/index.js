ec7bdf8f12706837a17c800cde8afc86547332b8
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-07-12 17:44:54 +0800

    add GlobalStyle & ThemeWrapper

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import GlobalStyle from './GlobalStyle/GlobalStyle';
+import ThemeWrapper from './GlobalStyle/ThemeWrapper';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeWrapper>
+      <GlobalStyle />
+      <App />
+    </ThemeWrapper>
   </React.StrictMode>,
   document.getElementById('root')
 );
