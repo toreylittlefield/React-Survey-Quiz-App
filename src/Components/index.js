87a79d0b2289da3240f3161c5637f6605745994c
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-07-12 11:15:32 +0800

    add QuizTitle component

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,2 +1,3 @@
 export { default as Loading } from './Loading/Loading';
 export { default as ProgressBar } from './ProgressBar/ProgressBar';
+export { default as QuizTitle } from './QuizTitle/QuizTitle';
