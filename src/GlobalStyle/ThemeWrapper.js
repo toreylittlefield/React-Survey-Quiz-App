d7455c370878915961ae21eeccff4acc10b9be30
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-07-13 15:45:47 +0800

    add boxShadowLight, boxShadowDark to theme

--- src/GlobalStyle/ThemeWrapper.js
+++ src/GlobalStyle/ThemeWrapper.js
@@ -5,6 +5,8 @@ const theme = {
   primaryFontColor: '#DBDDE5',
   primaryBgColor: '#363a66',
   secondaryBgColor: '#383D6E',
+  boxShadowLight: 'rgba(219, 221, 229, 0.2)',
+  boxShadowDark: 'rgba(54, 58, 102, 0.2)',
 };
 
 const ThemeWrapper = ({ children }) => (
