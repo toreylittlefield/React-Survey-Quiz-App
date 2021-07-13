8f31d408936634127ab6c266709353f25488057e
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-07-13 15:47:36 +0800

    add useref for QuizItems for scrolling

--- src/Components/QuizItems/QuizItems.js
+++ src/Components/QuizItems/QuizItems.js
@@ -1,23 +1,44 @@
-import React, { Children, cloneElement } from 'react';
+import React, { Children, cloneElement, useRef } from 'react';
 import QuizItemCard from './QuizItemCard';
 
 const QuizItems = ({ quizQuestions = [], children = [], ...props }) => {
+  const itemsRef = Array.from({ length: quizQuestions.length + 1 }, () =>
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useRef()
+  );
+  const handleShow = (i) => {
+    // itemsRef[i + 1].current.scrollIntoView({
+    //   behavior: 'smooth',
+    // });
+  };
+  const { choicesSelected = [{}] } = props;
   return (
     <React.Fragment>
       {quizQuestions.map((quizQuestion = {}, quizIdx) => {
         const { id, word, correctChoiceIndex } = quizQuestion;
         const childrenwithprops = Children.map(children, (element) =>
-          cloneElement(element, { ...{ ...props, ...quizQuestion, quizIdx } })
+          cloneElement(element, {
+            ...{ ...props, ...quizQuestion, ...choicesSelected, quizIdx },
+          })
         );
+
         return (
-          <QuizItemCard key={id + correctChoiceIndex}>
-            <h2>{word}</h2>
+          <QuizItemCard
+            key={id + correctChoiceIndex}
+            onClick={() => handleShow(quizIdx)}
+            choiceSelected={Object.entries(choicesSelected[quizIdx]).flat(2)}
+          >
+            <h2 ref={itemsRef[quizIdx]}>{word}</h2>
             {/* Quiz Questions */}
             {childrenwithprops}
             {/* End Quiz Questions */}
           </QuizItemCard>
         );
       })}
+      {/* <div
+        style={{ visibility: 'hidden' }}
+        ref={itemsRef[itemsRef.length - 1]}
+      ></div> */}
     </React.Fragment>
   );
 };
