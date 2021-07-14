25f6903b0f7c4b1d5c59e94bb212ba4e6f1e7f25
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-07-14 08:32:37 -0400

    add button colors ass hsl objects & add success

--- src/GlobalStyle/ThemeWrapper.js
+++ src/GlobalStyle/ThemeWrapper.js
@@ -3,10 +3,18 @@ import { ThemeProvider } from 'styled-components';
 
 const theme = {
   primaryFontColor: '#DBDDE5',
-  primaryBgColor: '#363a66',
-  secondaryBgColor: '#383D6E',
+  secondaryFontColor: '#7B7B7B',
+  primaryBgColor: '#141A33',
+  secondaryBgColor: '#222D58',
   boxShadowLight: 'rgba(219, 221, 229, 0.2)',
   boxShadowDark: 'rgba(54, 58, 102, 0.2)',
+  primaryButtonColor: { h: '189', s: '95%', l: '43%' },
+  secondaryButtonColor: { h: '228', s: '44', l: '24%' },
+  alternateButtonColor: { h: '0', s: '0%', l: '87%' },
+  whiteButtonColor: { h: '0', s: '0%', l: '100%' },
+  outlineColor: '#0D856E',
+  successColor: '#45C9AA',
+  rejectColor: '#C50E49',
 };
 
 const ThemeWrapper = ({ children }) => (
