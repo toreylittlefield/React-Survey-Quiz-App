4154ecec97c6c90f1405b4a49538b674d495fdac
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-07-14 08:36:13 -0400

    update the progress bar to display correct / false

--- src/Components/ProgressBar/ProgressBar.js
+++ src/Components/ProgressBar/ProgressBar.js
@@ -1,22 +1,61 @@
 import React from 'react';
 import Label from './Label';
-import Filler from './Filler';
+// import Filler from './Filler';
 import Container from './Container';
+import { useState, useEffect } from 'react';
+import { FaBone, GiFishbone, DiReact } from 'react-icons/all';
 
-const ProgressBar = ({ progress, quizQuestions }) => {
-  const calcProgress = () => Math.round((progress / quizQuestions) * 100);
+const ProgressBar = ({
+  progress = 0,
+  // quizQuestions = 0,
+  numCorrectAnswers = [],
+}) => {
+  // const calcProgress = () => Math.round((progress / quizQuestions) * 100);
 
-  const { completed } = {
-    completed: calcProgress(),
-  };
+  const [lines, setLines] = useState([
+    { character: '-', lineColor: 'inherit' },
+  ]);
+  useEffect(() => {
+    if (progress === 0)
+      return setLines(
+        numCorrectAnswers.map(() => ({
+          character: <DiReact />,
+          lineColor: 'inherit',
+        }))
+      );
+    numCorrectAnswers.map((answer, answerIdx) => {
+      if (answer === 1) {
+        setLines((prev) =>
+          prev.map((line, lineIdx) => {
+            if (lineIdx === answerIdx)
+              return { character: <FaBone />, lineColor: 'green' };
+            return line;
+          })
+        );
+      } else if (answer === 0) {
+        setLines((prev) =>
+          prev.map((line, lineIdx) => {
+            if (lineIdx === answerIdx)
+              return { character: <GiFishbone />, lineColor: 'red' };
+            return line;
+          })
+        );
+      }
+      return answer;
+    });
+  }, [numCorrectAnswers, progress]);
+
+  // const { completed } = {
+  //   completed: calcProgress(),
+  // };
 
   return (
     <section className="progress-bar">
-      <h3>{`Progress:`}</h3>
+      <h3>{`Score:`}</h3>
       <Container>
-        <Filler completed={completed}>
-          <Label>{`${completed}%`}</Label>
-        </Filler>
+        {lines.map((line) => (
+          <Label lineColor={line.lineColor}>{line.character}</Label>
+        ))}
       </Container>
     </section>
   );
