9c8a4eb55002c2c113393601df1cb451d729eb7f
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-07-15 09:31:33 -0400

    center text for QuizTitleH1 on mobile

--- src/Components/QuizTitle/QuizTitle.js
+++ src/Components/QuizTitle/QuizTitle.js
@@ -2,18 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 
 const QuizTitleSection = styled.section`
-  margin-bottom: 1em;
   display: flex;
   flex-direction: column;
-  padding: 2em;
-  align-items: flex-start;
-  width: 50vw;
+  justify-items: center;
+  /* align-items: center; */
+  width: min(100%, 50vw);
   max-width: 80%;
 `;
 
 const QuizTitleH1 = styled.h1`
   opacity: 0.8;
   letter-spacing: 0.4em;
+  @media (max-width: 480px) {
+    text-align: center;
+  }
 `;
 
 const QuizTitle = ({ title = '', children = [] }) => {
