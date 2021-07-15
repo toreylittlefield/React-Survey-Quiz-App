3fef613239bb0b7158b18c062e50941d70fbe214
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-07-15 09:30:30 -0400

    Container flex-box & center on mobile

--- src/Components/ProgressBar/Container.js
+++ src/Components/ProgressBar/Container.js
@@ -1,7 +1,13 @@
 import styled from 'styled-components';
 
 const containerStyles = styled.div`
-  padding: 2em 0em;
+  padding: 1em 0em;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  @media (max-width: 480px) {
+    justify-content: center;
+  }
 `;
 
 export default containerStyles;
