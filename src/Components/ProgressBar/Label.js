2ca96fdfb96322b24a03d91aa0d4b5ad1e43dfa4
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-07-15 09:30:48 -0400

    Label center self on mobile

--- src/Components/ProgressBar/Label.js
+++ src/Components/ProgressBar/Label.js
@@ -2,6 +2,9 @@ import styled from 'styled-components';
 
 const Label = styled.label`
   color: ${({ lineColor }) => lineColor};
+  @media (max-width: 480px) {
+    align-self: center;
+  }
 `;
 
 export default Label;
