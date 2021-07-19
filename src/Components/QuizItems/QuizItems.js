152b36be6a1fa2ca6db312759e1eaa2b8dd4608d
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-07-18 20:58:15 -0400

    comment out useRef

--- src/Components/QuizItems/QuizItems.js
+++ src/Components/QuizItems/QuizItems.js
@@ -1,4 +1,4 @@
-import React, { Children, cloneElement, useRef } from 'react';
+import React, { Children, cloneElement } from 'react';
 import QuizItemCard from './QuizItemCard';
 import styled from 'styled-components';
 
@@ -6,22 +6,29 @@ const QuizItemsContainer = styled.div`
   display: grid;
   width: 100%;
   place-items: center;
+  transform-style: preserve-3d;
+  perspective: 1500px;
+  backface-visibility: hidden;
 `;
 
 const QuizItems = ({ quizQuestions = [], children = [], ...props }) => {
-  const itemsRef = Array.from({ length: quizQuestions.length + 1 }, () =>
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useRef()
-  );
+  // const itemsRef = memo(
+  //   () =>
+  //     Array.from({ length: quizQuestions.length + 1 }, () =>
+  //       // eslint-disable-next-line react-hooks/rules-of-hooks
+  //       useRef()
+  //     ),
+  //   [quizQuestions]
+  // );
 
-  const handleShow = (i) => {
-    console.log({ itemsRef, ref: itemsRef[i + 1] });
-    setTimeout(() => {
-      itemsRef[i + 1].current.scrollIntoView({
-        behavior: 'smooth',
-      });
-    }, 400);
-  };
+  // const handleShow = (i) => {
+  //   console.log({ itemsRef, ref: itemsRef[i + 1] });
+  //   setTimeout(() => {
+  //     itemsRef[i + 1].current.scrollIntoView({
+  //       behavior: 'smooth',
+  //     });
+  //   }, 400);
+  // };
   const { choicesSelected = [{ key: '' }], showAnswers = false } = props;
 
   return (
@@ -37,26 +44,27 @@ const QuizItems = ({ quizQuestions = [], children = [], ...props }) => {
           <QuizItemCard
             // ref={itemsRef[quizIdx]}
             key={id + correctChoiceIndex}
-            onClick={(e) => {
-              console.log({ target: e.target });
-              handleShow(quizIdx);
-            }}
+            // onClick={(e) => {
+            //   console.log({ target: e.target });
+            //   handleShow(quizIdx);
+            // }}
             choiceSelected={Object.entries(choicesSelected[quizIdx]).flat(2)}
             cardNumber={quizIdx}
             correctChoiceIndex={correctChoiceIndex}
             showAnswers={showAnswers}
           >
-            <h2 ref={itemsRef[quizIdx]}>{word}</h2>
+            {/* ref={itemsRef[quizIdx]} */}
+            <h2>{word}</h2>
             {/* Quiz Questions */}
             {childrenwithprops}
             {/* End Quiz Questions */}
           </QuizItemCard>
         );
       })}
-      <div
+      {/* <div
         style={{ visibility: 'hidden' }}
         ref={itemsRef[itemsRef.length - 1]}
-      ></div>
+      ></div> */}
     </QuizItemsContainer>
   );
 };
